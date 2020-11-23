7819a75ce958953cbed65579191d996b2231ee48
gaalgery <56272450+gaalgery@users.noreply.github.com>
2020-11-23 14:48:18 +0100

    javítás

---
+++ src/app/loggingout-snackbar/loggingout-snackbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loggingout-snackbar',
+  templateUrl: './loggingout-snackbar.component.html',
+  styleUrls: ['./loggingout-snackbar.component.css']
+})
+export class LoggingoutSnackbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
